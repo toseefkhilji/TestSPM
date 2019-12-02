5f4c90e319d04d6fd3ec363aacec3faf6d5872ac
Toseefhusen <toseefhusen@webteamcorp.com>
2019-12-02 18:38:28 +0530

    initial

---
+++ Sources/TestSPM/TestSPM.swift
@@ -0,0 +1,10 @@
+struct TestSPM {
+    var text = "Hello, World!"
+}
+
+
+public class Converter {
+    static func convert(cm: Float) -> Float {
+        return cm * 0.1
+    }
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+import TestSPMTests
+
+var tests = [XCTestCaseEntry]()
+tests += TestSPMTests.__allTests()
+
+XCTMain(tests)
