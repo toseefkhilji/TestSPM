c0370ca8e24a11a2fdfb908d1ac1d61e0606c791
Toseef <toseefkhilji@users.noreply.github.com>
2019-12-02 18:47:58 +0530

    make public

--- Sources/TestSPM/TestSPM.swift
+++ Sources/TestSPM/TestSPM.swift
@@ -1,10 +1,10 @@
-struct TestSPM {
+public struct TestSPM {
     var text = "Hello, World!"
 }
 
 
 public class Converter {
-    static func convert(cm: Float) -> Float {
+    public static func convert(cm: Float) -> Float {
         return cm * 0.1
     }
 }
